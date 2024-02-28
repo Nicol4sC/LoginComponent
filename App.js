d21c1ac10f2b4b663f9c87ae1589e319ea2692fb
Nicolas Cardozo <125682448+Nicol4sC@users.noreply.github.com>
2024-02-28 14:57:45 -0300

    a

--- App.js
+++ App.js
@@ -4,8 +4,23 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontSize: 30, }}>Login</Text>
+            <TextInput style={{ height: 70, width: 300, border: 'solid', borderWidth: 1, borderColor: 'white', margin: '10px', color: 'white' }}
+                placeholder='email'
+                inputMode='email'
+                color='white'
+            />
+
+            <TextInput style={{ height: 70, width: 300, border: 'solid', borderWidth: 1, borderColor: 'white', margin: '10px', color: 'white'}}
+                placeholder='senha'
+                inputMode='none'
+                secureTextEntry={true}
+                color='white'
+            />
+            <Button style={styles.btn}
+                onPress={click}
+                title="Entrar"
+                color="black" />
     </View>
   );
 }
